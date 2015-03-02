11975aed96df0c5a34e17015ae13412a9c43e78f
Serg A. Osipov (attava) <serg.osipov@gmail.com>
2015-03-02 16:04:58 +1000

    modified:   db.js

---
+++ db.js
@@ -0,0 +1,95 @@
+/** @module media
+ * @name db
+ * @author Serg A. Osipov
+ * @email serg.osipov@gmail.com
+ * @overview Module for make DB-connection and use it
+ */
+
+//var pg = require('pg').native;
+var pg = require('pg');
+var RSVP = require('rsvp');
+var client;
+
+module.exports = {
+/**
+ * Вернуть объект "клиент" 
+ * @param  {string} conString Строка подключения
+ * @return {object}           Клиент
+ */
+	getClient: function(conString){
+		client=client || new pg.Client(conString);
+		return (client);
+	},
+
+/**
+ * Сбросить клиента
+ * @return {undefined} 
+ */
+	clearClient: function(){
+		client=client || new pg.Client(conString);
+		client.end();
+	},
+
+/**
+ * Установить соединение с базой, используя строку подключения connectionstring 
+ * @param  {object}		connectionstring Объект, описывающий параметры подклчюения (свойство connectionstring)
+ * @return {object}		Клиент
+ */
+	connect: function(connectionstring){
+		return new RSVP.Promise(function(resolve, reject){
+			var dbclient=module.exports.getClient(connectionstring);
+			dbclient.connect(function(err){
+				if(err){
+					dbclient.end();
+					module.exports.clearClient();
+					reject(err);
+				}else{
+					resolve(dbclient);
+				};
+			});
+		});
+	},
+
+/**
+ * Выполнение SQL-запроса с возвратом результатов через callback
+ * @param  {str} 			SQL-запрос
+ * @param  {Array} 			values	Список параметров
+ * @param  {function}		onok 	Callback вызываемый при успешном выполнении запроса. Параметр - результат выполнения.
+ * @param  {function}		onerr 	Callback вызываемый при возникновении ошибки. Параметр - ошибка.
+ * @return {undefined}        
+ */
+	sql: function(sql, values, onok, onerr){
+		client.query(sql, values, function(err, result){
+			if(err){
+				err.number=103;
+				err.sql=sql;
+				err.values=values;
+				if(typeof(onerr)=='function'){
+					onerr(err);
+				};
+			}else{
+				onok(result);
+			};
+		});
+	},
+
+/**
+ * Выполнение SQL-запроса с возвратом результатов через promise
+ * @param 	{String}		sql		SQL-запрос
+ * @param 	{Array} 		values	Список параметров
+ * @return 	{Promise}		Промайс-объект, resolve-метод, который получит на вход результат выполнения запроса.
+ */
+	SQL: function(sql, values){
+		return new RSVP.Promise(function(resolve, reject){
+			client.query(sql, values, function(err, result){
+				if(err){
+					err.sql=sql;
+					err.values=values;
+					reject(err);
+				}else{
+					resolve(result);
+				};
+			});
+		});
+	}
+};
